63e77515d6f03533e171113a39e4bda060f3a2e1
Filipe Hanniel <filipehanniel@gmail.com>
2023-07-18 21:49:35 -0300

    exercicio 02 concluido

--- src/exercicio01.java
+++ src/exercicio01.java
@@ -4,8 +4,8 @@ public class exercicio01 {
     public static void main(String[] args) {
 
         char sexo;
-        float altura = 0, maiorAltura, menorAltura, mediaAlturaMulheres;
-        int numHomens;
+        float altura = 0, maiorAltura = -999999, menorAltura = 999999, somaAlturaMulheres = 0, mediaMulheres = 0;
+        int numHomens = 0, numMulheres = 0;
 
         Scanner sc = new Scanner(System.in);
 
@@ -25,9 +25,26 @@ public class exercicio01 {
                     System.out.println("Por gentileza digite um valor válido para o sexo do indivíduo!!!");
                 }
             } while (sexo != 'm' & sexo != 'f');
-
+            if (altura > maiorAltura) {
+                maiorAltura = altura;
+            }
+            if (altura < menorAltura) {
+                menorAltura = altura;
+            }
+            if (sexo == 'f') {
+                somaAlturaMulheres = altura;
+                numMulheres += 1;
+            } else {
+                numHomens += 1;
+            }
         }
-
+        if (numMulheres != 0) {
+            mediaMulheres = somaAlturaMulheres / mediaMulheres;
+        }
+        System.out.println("A maior altura do grupo é: " + maiorAltura);
+        System.out.println("A menor altura do grupo é: " + menorAltura);
+        System.out.println("A média de altura das mulheres é : " + mediaMulheres );
+        System.out.println("A quantidade de homens é de: " + numHomens);
         sc.close();
     }
 
---
+++ src/exercicio02.java
@@ -0,0 +1,12 @@
+public class exercicio02 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("=== CONVERSÃO EM CELCIUS ===");        
+
+        for (int i = 50; i < 151; i++) {             
+            System.out.println(i + "º Fahrenheit = " +  (5.0/9.0)*(i-32) + "º Celcius");                        
+        }
+        
+    }
+}
